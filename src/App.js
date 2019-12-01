8d552895016834a5dfc1f918ff19c93e5d722bc1
okdohyuk <okdohyuk2@gmail.com>
2019-12-01 15:26:48 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,67 +1,34 @@
 import React from 'react';
-import PropTypes  from "prop-types";
-import './app.css';
 
-
-const foodILike = [
-  {
-    id: 1,
-    name: "guksu",
-    image:
-    "https://post-phinf.pstatic.net/MjAxODAzMTJfMzcg/MDAxNTIwODE2MzIzMDky.ohBW46GZDFcB6b04P5RC5jlIfUqKPQK0J1lQ-d4EdXgg.1SZqEdGiBmuWKI-Q1hqz7XABc97-9z4kRVCopXBlrtwg.JPEG/Z16A3048sat2.jpg?type=w1200",
-    rating: 10
-  },
-  {
-    id: 2,
-    name: "hamburg",
-    image:
-    "https://mblogthumb-phinf.pstatic.net/20151103_204/tdepidostv_1446534240529zXSVF_JPEG/%C7%DC%B9%F6%B0%C5_%C1%D6%B9%AE%C7%D2%B6%A7_%C6%C1_%285%29.jpg?type=w2",
-    rating: 4.9
-  },
-  {
-    id: 3,
-    name: "chicken",
-    image:
-    "https://img1.daumcdn.net/thumb/R720x0/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2FSNUH%2Fea3136eda7e44fd4a423e78efff8e3ec.jpg",
-    rating: 4.8
-  },
-  {
-    id: 4,
-    name: "pizza",
-    image: "http://www.pizzabig.co.kr/theme/basic/img/sub0201_img03.jpg",
-    rating: 5.0
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("Component rendered");
   }
-]
-
-function Food({ name, picture, rating }){
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+  componentWillUnmount() {
+    console.log("Goodbye, this world");
+  }
+  render(){
+    console.log("I'm rendering");
   return (
-    <div className="menu">
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} height="100px" />
-    </div>
-  );
-}
-
-Food.prototype = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function App() {
-  return (
-    <div>
-    {foodILike.map(dish => (
-      <Food 
-      key={dish.id} 
-      name={dish.name} 
-      picture={dish.image} 
-      rating={dish.rating}
-      />
-      ))}
+  <div>
+    <h1>The number is: {this.state.count}</h1>
+    <button onClick={this.add}>Add</button>
+    <button onClick={this.minus}>Minus</button>
   </div>
-  );
+  )
+  }
 }
 
 export default App;
