ef724fcfa5bff7aaa204579388de68f0da96f083
okdohyuk <okdohyuk2@gmail.com>
2019-12-18 11:33:33 +0900

    Updates

---
+++ precache-manifest.b53b844d606ed6c4a7c3b0668943deb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b05b4585a04aab28256194283338644",
+    "url": "/react-yes/index.html"
+  },
+  {
+    "revision": "3876b6693974dc46c167",
+    "url": "/react-yes/static/css/main.3d10e777.chunk.css"
+  },
+  {
+    "revision": "67f31a2ba2a9dce25aee",
+    "url": "/react-yes/static/js/2.cff5d42e.chunk.js"
+  },
+  {
+    "revision": "3876b6693974dc46c167",
+    "url": "/react-yes/static/js/main.0894489c.chunk.js"
+  },
+  {
+    "revision": "1626f4eb30fd33176355",
+    "url": "/react-yes/static/js/runtime-main.38c64ac2.js"
+  }
+]);
